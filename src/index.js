a46b087b54b3cdf7ad2254b3d2621a2093c8cf13
Wellington Mesquita <wellingtomdev@gmail.com>
2022-03-05 22:38:20 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 
