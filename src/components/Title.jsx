82d86a8cb7a6fb5cbd6fb026a9a52e8f05cc260b
Wellington Mesquita <wellingtomdev@gmail.com>
2022-03-05 22:32:02 -0300

    first

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+
+
+export default props => {
+
+    return (
+        <div className="title">
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
+        </div>
+    )
+
+}
